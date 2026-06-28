e68c084d71d37d29f5cabc36ad05c6a47dfce781
Luis Gustavo Pacheco <gustavogla@gmail.com>
2026-06-28 17:09:25 -0300

    Update notion.js

--- api/notion.js
+++ api/notion.js
@@ -252,7 +252,7 @@ module.exports = async function handler(req, res) {
         if (dados.gerente) props['Gerente Demandante'] = { rich_text: [{ text: { content: dados.gerente } }] };
         if (dados.fiscal) props['Fiscal do Contrato'] = { rich_text: [{ text: { content: dados.fiscal } }] };
         if (dados.observacoes) props['Observações'] = { rich_text: [{ text: { content: dados.observacoes } }] };
-        if (dados.data_email) props['Data E-mail Recebido'] = { date: { start: dados.data_email } };
+        if (dados.data_email) props['date:Data E-mail Recebido:start'] = { date: { start: dados.data_email } };
 
         var r = await fetch('https://api.notion.com/v1/pages', {
           method: 'POST',
@@ -271,11 +271,11 @@ module.exports = async function handler(req, res) {
         if (dados.gerente !== undefined) props2['Gerente Demandante'] = { rich_text: dados.gerente ? [{ text: { content: dados.gerente } }] : [] };
         if (dados.fiscal !== undefined) props2['Fiscal do Contrato'] = { rich_text: dados.fiscal ? [{ text: { content: dados.fiscal } }] : [] };
         if (dados.observacoes !== undefined) props2['Observações'] = { rich_text: dados.observacoes ? [{ text: { content: dados.observacoes } }] : [] };
-        if (dados.data_curriculos !== undefined) props2['Data Currículos Enviados'] = { date: dados.data_curriculos ? { start: dados.data_curriculos } : null };
-        if (dados.data_entrevista !== undefined) props2['Data Pedido Entrevista'] = { date: dados.data_entrevista ? { start: dados.data_entrevista } : null };
-        if (dados.data_escolhido !== undefined) props2['Data Candidato Escolhido'] = { date: dados.data_escolhido ? { start: dados.data_escolhido } : null };
-        if (dados.data_autorizacao !== undefined) props2['Data Autorização'] = { date: dados.data_autorizacao ? { start: dados.data_autorizacao } : null };
-        if (dados.data_admissao !== undefined) props2['Data Admissão'] = { date: dados.data_admissao ? { start: dados.data_admissao } : null };
+        if (dados.data_curriculos !== undefined) props2['date:Data Currículos Enviados:start'] = { date: dados.data_curriculos ? { start: dados.data_curriculos } : null };
+        if (dados.data_entrevista !== undefined) props2['date:Data Pedido Entrevista:start'] = { date: dados.data_entrevista ? { start: dados.data_entrevista } : null };
+        if (dados.data_escolhido !== undefined) props2['date:Data Candidato Escolhido:start'] = { date: dados.data_escolhido ? { start: dados.data_escolhido } : null };
+        if (dados.data_autorizacao !== undefined) props2['date:Data Autorização:start'] = { date: dados.data_autorizacao ? { start: dados.data_autorizacao } : null };
+        if (dados.data_admissao !== undefined) props2['date:Data Admissão:start'] = { date: dados.data_admissao ? { start: dados.data_admissao } : null };
 
         var r2 = await fetch('https://api.notion.com/v1/pages/' + pid, {
           method: 'PATCH',
